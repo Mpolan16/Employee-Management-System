0267a2383cddb497547c378eeaeb33b31e76d47b
Mónica Polanco <mpolan16@g.holycross.edu>
2020-05-29 12:30:25 -0400

    set up the sql table

---
+++ employee_data.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE database employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE Employee_role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,4) NULL,
+  department_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE Employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+select * from Employee_role;
+select * from Employee;
