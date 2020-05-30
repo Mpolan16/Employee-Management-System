41c11d21664abbe6ea030255583d80ebcdabfcbb
Mónica Polanco <mpolan16@g.holycross.edu>
2020-05-30 12:34:27 -0400

    created add employee function

--- employeeDB.sql
+++ employeeDB.sql
@@ -3,7 +3,7 @@ CREATE database employeeDB;
 
 USE employeeDB;
 
-CREATE TABLE department (
+CREATE TABLE Department (
   id INT NOT NULL AUTO_INCREMENT,
   department_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
@@ -11,7 +11,7 @@ CREATE TABLE department (
 CREATE TABLE Employee_role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NULL,
-  salary DECIMAL(10,4) NULL,
+  salary DECIMAL NULL,
   department_id INT NULL,
   PRIMARY KEY (id)
 );
@@ -24,6 +24,16 @@ CREATE TABLE Employee (
   PRIMARY KEY (id)
 );
 
-SELECT * FROM department;
+SELECT * FROM Department;
 select * from Employee_role;
 select * from Employee;
+
+INSERT INTO Department (department_name)
+VALUES ("sales"),("engineering"),("HR");
+
+INSERT INTO Employee_role (title, salary, department_id)
+VALUES ("sales manager", 80000,1),("lead engineer", 170000,2),("sales associate", 45000,1),("technician", 85000,2);
+
+INSERT INTO Employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ashley", "Cruz", 2,null),("carmelo", "Smith",4,1),("Henry","Lopez",1,null),("Mabel","Diaz",3,3);
+
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const mysql = require("mysql");
-const inquirer = require("inquire");
+const inquirer = require("inquirer");
+const util = require("util");
 
 let connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Mp01151994!",
+  password: "bc2020",
   database: "employeeDB"
 });
+
+connection.query = util.promisify(connection.query);
 
 connection.connect(function(err) {
   if (err) throw err;
@@ -20,26 +23,94 @@ function start() {
             name: "initiate",
             type: "list",
             message: "Would you like to do?",
-            choices: ["Add Employee", "View Departments", "Update Employee Roles","EXIT"]
+            choices: ["Add Employee","Add Role","Add Department", "View Departments","View Roles","View Employees", "Update Employee Roles","EXIT"]
       }).then(function(answer) {
-        if (answer.initiate === "Add Employee"){
-            addEmployee();
-        }
-        else if (answer.initiate === "View Departments"){
-            viewDepartments();
-        }
-        else if (answer.initiate === "Update Employee Roles"){
-            updateRoles();
-        }
-        else{
-            connection.end();
-        }
-      });
+        switch (answer.initiate){
+            case "Add Employee":
+                addEmployee();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "Add Department":
+                addDepartment();
+                break;
+            case "View Departments":
+                viewDepartments();
+                break;
+            case "View Roles":
+                viewRoles();
+                break;
+            case "View Employees":
+                viewEmployees();
+                break;
+            case "Update Employee Roles":
+                updateRoles();
+                break;
+            case "EXIT":
+                connection.end();            
+      }});
 }
 
-function addEmployee() {
+async function addEmployee() {
+    let roles = await connection.query("SELECT id, title FROM Employee_role");
+    let employees = await connection.query("SELECT * FROM Employee")
+    console.log(roles);
+    console.log(employees);
+    let roleChoices = roles.map(function(role) {
+        return{
+        name:role.title,
+        value:role.id
+        }
+    })
+    console.log(roleChoices);
+    let managerChoices = employees.map(function(employee){
+        return{
+            name:employee.first_name+" "+employee.last_name,
+            value:employee.id
+        }
+    })
+    //will return an array of objt that constain the id and title of the role. I need to grab the id and table and concat into a single string to push into an array. (forloop,.map.foreach).the array of string will be the choices in the prompts. once you get answer from user use .split to just get the id portion.
+    //for the manager let manager = and get new array back similiar to the id title on, but you need a null option (using .map may be tricky look at unshift)
+    inquirer
+        .prompt([
+            {
+                name: "first_name",
+                type: "input",
+                message: "What is the employees first name?"
+            },
+            {
+                name: "last_name",
+                type: "input",
+                message: "What is the employees last name?"
+            },
+            {
+                name: "role_id",
+                type: "list",
+                message: "What is the employees role?",
+                choices: roleChoices
+            },  
+            {
+                name: "manager_id",
+                type: "list",
+                message: "Who is the manager?",
+                choices: managerChoices
+            }
+        ]).then((answers)=>{
+            connection.query("INSERT INTO Employee SET ?", answers);
+            console.log(answers)
+        })
 }
-function viewDepartments() {
+
+function addRole(){
+}
+function addDepartment() {
+}
+function viewDepartments(){
+}
+function viewRoles() {
+}
+function viewEmployees(){
 }
 function updateRoles() {
 }
